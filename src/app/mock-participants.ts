4469a7d441a10bb00caf1971317ba351ebc23e1b
“karimshakir” <karimshakir99@gmail.com>
2019-12-06 21:08:28 -0500

    create mock-participant file

---
+++ src/app/mock-participants.ts
@@ -0,0 +1,19 @@
+import { Participant } from './models/participant';
+
+export const PARTICIPANTS: Participant[] = [
+  {
+    userId: 1;
+    eventId: 1;
+    dish: dish1;
+  },
+  {
+    userId: 2;
+    eventId: 2;
+    dish: dish2;
+  },
+  {
+    userId: 3;
+    eventId: 3;
+    dish: dish3;
+  },
+];
